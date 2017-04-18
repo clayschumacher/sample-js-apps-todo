116ec7b3b8dd480b3dcfd108c6f80bb0e8811ff6
Clay Schumacher <clayschumacher@iseinc.biz>
2017-04-18 01:30:02 -0500

    first commit

---
+++ ng2/app/app.component.js
@@ -0,0 +1,84 @@
+(function(app) {
+  app.AppComponent =
+    ng.core.Component({
+      selector: 'my-app',
+      template: `<!-- navigation bar -->
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <a class="navbar-brand"><span class="glyphicon glyphicon-time"></span> ToDo - Angular</a>
+            <h4 class="navbar-text navbar-right"> <span class="glyphicon glyphicon-user"></span> {{ user.firstName }} &nbsp;</h4>
+        </div>
+    </nav>
+
+        <!-- show the tasks -->
+        <div class="table-responsive">
+            <table class="table table-hover">
+                <thead>
+                    <!--<tr>
+                        <th></th>
+                        <th>Task</th>
+                        <th></th>
+                    </tr>-->
+                    <tr>
+                        <th></th>
+                        <th><input type="text" class="form-control" placeholder="Task" [(innerText)]="task.name" onkeyup="keyAddTask(event)"></th>
+                        <th>
+                            <button class="btn btn-s" (click)="addTask">
+                                <span class="glyphicon glyphicon-plus-sign"></span>
+                            </button>
+                        </th>
+                </thead>
+                <tbody>
+                    <tr *ngFor="let task of tasks">
+                        <td>
+                            <span class="glyphicon glyphicon-check"></span>
+                        </td>
+                        <td>{{ task.name }}</td>
+                        <td>
+                            <button class="btn btn-s" (click)="deleteEvent($index)">
+                                <span class="glyphicon glyphicon-ok"></span>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>`,
+        directives: [ng.common.NgFor]
+    })
+    .Class({
+      constructor: function() {
+          this.task = { name: '' };
+          this.tasks = [];
+          this.user = { username: '', firstName: 'Clay', lastName: 'Schumacher' };
+      },
+      fetchTasks: function() {
+          var tasks = [
+                {
+                    id: 1,
+                    name: 'Buy Milk'
+                },
+                {
+                    id: 2,
+                    name: 'Find Keys'
+                },
+                {
+                    id: 3,
+                    name: 'Drive to Store'
+                }
+            ];
+            this.tasks.push(tasks);
+      },
+      addTask: function() {
+          if (this.task.name) {
+              this.tasks.push(this.task);
+              this.task = { name: '' };
+            }
+      },
+      deleteEvent: function(index) {
+          this.tasks.splice(index, 1);
+      },
+      ngOnInit: function() {
+          this.fetchTasks();
+      }
+    });
+})(window.app || (window.app = {}));
---
+++ vue/app.js
@@ -0,0 +1,78 @@
+// Register a global custom directive called v-focus
+Vue.directive('focus', {
+  // When the bound element is inserted into the DOM...
+  inserted: function (el) {
+    // Focus the element
+    el.focus()
+  }
+});
+
+var app = new Vue({
+    el: '#app',
+    // Here we can register any values or collections that hold data
+    // for the application
+    data: {
+        task: { name: '' },
+        tasks: [],
+        displayDate: '',
+        user: { username: '', firstName: 'Clay', lastName: 'Schumacher'}
+    },
+
+    // Anything within the ready function will run when the application loads
+    mounted: function () {
+        this.$nextTick(function() {
+            // When the application loads, we want to call the method that initializes
+            // some data
+            this.fetchTasks();
+            var date = new Date();
+            var options = { weekday: "short", year: "numeric", month: "short", day: "numeric" };
+            this.displayDate = date.toLocaleDateString("en-us", options);
+        });
+    },
+
+    // Methods we want to use in our application are registered here
+    methods: {
+        // We dedicate a method to retrieving and setting some data
+        fetchTasks: function () {
+            var tasks = [
+                {
+                    id: 1,
+                    name: 'Buy Milk'
+                },
+                {
+                    id: 2,
+                    name: 'Find Keys'
+                },
+                {
+                    id: 3,
+                    name: 'Drive to Store'
+                }
+            ];
+            // set is a convenience method provided by Vue that is similar to pushing
+            // data onto an array
+            Vue.set(this, 'tasks', tasks);
+        },
+
+        // Adds a task to the existing tasks array
+        addTask: function () {
+            if (this.task.name) {
+                this.tasks.push(this.task);
+                this.task = { name: '' };
+            }
+        },
+
+        deleteEvent: function (index) {
+            if (confirm("Are you sure you want to mark complete?")) {
+                // $remove is deprecated in Vue 2.0, splice without a replacement list can be used.
+                this.tasks.splice(index, 1);
+            }
+        }
+
+    }
+})
+
+function keyAddTask(event) {
+    if (event.keyCode === 13) {
+        app.addTask();
+    }
+}
